78a88a412c3ac1525fe24fefc44ae683c926a4a8
BillClintonAglomasa <aglomasa@yahoo.com>
2023-12-09 00:43:38 +0000

    Adding script using the fork to create a child process

--- execve.c
+++ execve.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
+	pid_t status;
 	int returned_fork;
 	char *argv[] = {"/bin/ls", "-l", "/home/vagrant/learnings/simple_shell", NULL};
 
@@ -20,9 +22,13 @@ int main(void)
 	if (returned_fork == 0)
 	{
 		printf("Fork is succesful");
+		execve(argv[0], argv, NULL);
 	}
 
 	else
 	{
-		
+		wait(&status);
+		printf("Waiting for child process to finish");
 	}
+	exit(EXIT_SUCCESS);
+}
