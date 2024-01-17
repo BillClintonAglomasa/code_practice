96c282b5010c8fa93b2e6d0b7a7e049157ffb20e
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-17 22:55:26 +0000

    Add count_node function to count nodes in singly linked list

---
+++ C_projects/list/single_link/count_node.c
@@ -0,0 +1,31 @@
+#include LIST_FUNCTIONS
+
+/**
+ * count_node: Count the number of nodes in a singly linked list
+ * 
+ * @head: A header pointer to the singly linked list
+ *
+ * Return: Returns Nothing
+ */
+
+void count_node(struct node *head)
+{
+  int i;
+
+  if (head == NULL)
+    {
+      printf("Singly linked list is empty");
+    }
+
+  struct node *ptr = head;
+
+  i = 0;
+  while (ptr != NULL)
+    {
+      i++;
+      ptr = ptr->link;
+    }
+
+  printf("%d", i);
+  
+}
