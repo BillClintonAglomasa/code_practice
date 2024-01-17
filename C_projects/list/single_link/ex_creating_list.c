b0762553c178b69234342f986a7db7135fc7c5fe
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-17 21:15:49 +0000

    Add a file that creates a linked list

---
+++ C_projects/list/single_link/ex_creating_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: Create a linked list
+ */
+
+struct node
+{
+  int a;
+  struct node *link;
+};
+
+int main(void)
+{
+  struct node *head = NULL;
+  head = malloc(sizeof(struct node));
+  head->a = 45;
+  head->link = NULL;
+
+  printf("%d\n", head->a);
+  
+  return(0);
+}
