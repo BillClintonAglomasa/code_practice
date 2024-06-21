a7f3e9f4d709e32587c9efa8408df711470c38e2
BillClintonAglomasa <aglomasa@yahoo.com>
2024-06-21 00:12:01 +0000

    Completed escape sequence use in ex10

---
+++ SE/Python/Learn_Python_Hardway/Exercise_1/ex10_experiment.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+#Experimenting with escape sequences
+print("This is a backslash \\ in python")
+print("This is a single quote \' in python")
+print('This is a double quote \" in python')
+print("This is the ASCII bell \a in python")
+print("This is the ASCII formfeed \f in python")
+print("This is the ASCII linefeed \n in python")
+#print("This is the character named, name \N{name}")
+print("This is the carriage return \r in python")
+print("This is the horizontal tab \t in python")
+print("This is a character with 16-bit hex value xxxx \Uxxxx in python")
+print("This is a character with 32-bit hex value xxxxxxxx \Uxxxxxxxx in python")
+print("This is the ASCII vertical tab \v in python")
+print("This is a character with octal value ooo")
+print("This is a character with hex value hh \xhh character")
