46537e1641d2a1b1c46693636a42e0ed84957381
BillClintonAglomasa <aglomasa@yahoo.com>
2024-07-03 20:54:38 +0000

    Completed ex35 drill 5 of the book

--- SE/Python/Learn_Python_Hardway/Part_2/ex35_1.py
+++ SE/Python/Learn_Python_Hardway/Part_2/ex35_1.py
@@ -3,15 +3,9 @@
 from sys import exit
 
 def gold_room():
-    print("This room is full of gold. How much do you take?")
+    choice = int(input("This room is full of gold. How much do you take? "))
 
-    choice = input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number.")
-
-    if how_much < 50:
+    if choice < 50:
         print("Nice, you're not greedy, you win!")
         exit(0)
     else:
@@ -19,10 +13,12 @@ def gold_room():
 
         
 def bear_room():
-    print("There is a bear here.")
-    print("The bear has a bunch of honey.")
-    print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
+    print("""There is a bear here.
+    The bear has a bunch of honey.
+    The fat bear is in front of another door.
+    How are you going to move the bear?
+    Will you either 'take honey', 'taunt bear', 'open door' or do something else?\n""")
+
     bear_moved = False
 
     while True:
@@ -35,18 +31,22 @@ def bear_room():
             print("The bear has moved from the door.")
             print("You can go through it now.")
             bear_moved = True
+
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
+
         elif choice == "open door" and bear_moved:
             gold_room()
+
         else:
             print("I got no ideas what that means.")
 
 
 def cthulhu_room():
-    print("Here you see the great evil Cthulhu.")
-    print("He, it, whatever stare at you and you go insane.")
-    print("Do you flee for your life or eat your head?")
+    print("""Here you see the great evil Cthulhu.
+    He, it, whatever stare at you and you go insane.
+    Choose either of the words in quotation marks.
+    Do you 'flee' for your life or eat your 'head' or you have 'ideas'?\n""")
 
     choice = input("> ")
 
@@ -63,9 +63,9 @@ def dead(why):
     exit(0)
 
 def start():
-    print("You are in a dark room.")
-    print("There is a door to your right and left.")
-    print("Which one do you take?")
+    print("""You are in a dark room.
+    There is a door to your right and left.
+    Which one do you take?\n""")
 
     choice = input("> ")
 
@@ -76,5 +76,4 @@ def start():
     else:
         dead("You stumble around the room until you starve.")
 
-
-start()
+start_room()
