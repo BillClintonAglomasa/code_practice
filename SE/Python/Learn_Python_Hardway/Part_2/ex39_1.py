db593f6e8245cb53717155fba4b6798629f862ae
BillClintonAglomasa <aglomasa@yahoo.com>
2024-07-07 21:22:08 +0000

    Updated ex38_1 with to new methods

--- SE/Python/Learn_Python_Hardway/Part_2/ex39_1.py
+++ SE/Python/Learn_Python_Hardway/Part_2/ex39_1.py
@@ -5,7 +5,16 @@ regions = {
     'Greater-Accra': 'GAA', 'Eastern': 'KOF', 'Ashanti': 'ASH', 'Central': 'CAP', 'Volta': 'HO'
 }
 
+# Using the list function with dictionaries
 print(regions.get('Volta'))
 print(regions.items())
 for region , abbrev in list(regions.items()):
     print(f"The region {region} has the following abbreviation: {abbrev}")
+
+# Using dictionaries withou the list function
+for region, abbrev in regions.items():
+    print(f"{region} has the abbreviation {abbrev}")
+
+# Using enumerate to get the index of keys in a dictionary 
+for region, abbrev in enumerate(regions):
+    print(abbrev, region)
