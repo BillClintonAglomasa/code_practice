71fe2bb5cbd105a81e6c7e1fc5a4d61fb4fb5881
BillClintonAglomasa <aglomasa@yahoo.com>
2024-08-02 14:57:17 +0000

    Completed ex44b

---
+++ SE/Python/Learn_Python_Hardway/Part_2/ex44b.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+class Parent(object):
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+class Child(Parent):
+
+    def override(self):
+        print("CHILD override()")
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
