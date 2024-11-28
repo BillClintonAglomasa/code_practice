6bc057e14b5140e4cce8b6062a962d961fdc9e8a
BillClintonAglomasa <aglomasa@yahoo.com>
2024-11-28 22:01:24 +0000

    Program to convert fahrenheit to celsius using symbolic constants in a for loop

---
+++ SE/C_programming/Chapter_1/ex2.5_symbolic_constants.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**Convert celsius to fahrenhiet and employing
+  *symbolic constants
+  */
+
+#define UPPER 300
+#define STEP  20
+#define LOWER 0
+
+int main(void)
+{
+  /** Defining variables*/
+  float celsius, fahr;
+
+  /** Printing header*/
+  printf("%s %s\n", "Celsius", "Fahrenheit");
+
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+    {
+      celsius = (5.0 / 9.0) * (fahr - 32);
+      printf("%3.0f %12.2f\n", celsius, fahr);
+    }
+  return 0;
+}
