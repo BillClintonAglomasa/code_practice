829e25b290468cf0d4110e5ef1bf50efc808bc18
BillClintonAglomasa <aglomasa@yahoo.com>
2024-12-12 12:53:21 +0000

    A program that copies a character from input to output

--- SE/C_programming/Chapter_1/ex2.4_symbolic_constants.c
+++ SE/C_programming/Chapter_1/ex2.4_symbolic_constants.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-/**Convert celsius to fahrenhiet and employing
-  *symbolic constants
-  */
+/**
+ *Convert celsius to fahrenhiet and employing
+ *symbolic constants
+ */
 
 #define UPPER 300
 #define STEP  20
@@ -10,16 +11,15 @@
 
 int main(void)
 {
-  /** Defining variables*/
-  float celsius, fahr;
+/** Defining variables*/
+	float celsius, fahr;
 
-  /** Printing header*/
-  printf("%s %s\n", "Celsius", "Fahrenheit");
-
-  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
-    {
-      celsius = (5.0 / 9.0) * (fahr - 32);
-      printf("%3.0f %12.2f\n", celsius, fahr);
-    }
-  return 0;
+/** Printing header */
+	printf("%s %s\n", "Celsius", "Fahrenheit");
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+	{
+		celsius = (5.0 / 9.0) * (fahr - 32);
+		printf("%3.0f %12.2f\n", celsius, fahr);
+	}
+	return (0);
 }
--- SE/C_programming/Chapter_1/ex3.0_file_copy.c
+++ SE/C_programming/Chapter_1/ex3.0_file_copy.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 /**
- * Copy from input to output
+ * main - Entry point of program
+ *
+ * Return: Always 0 (success)
+ *
+ * Description: A program that copies from input to output
  */
 
-int main(void){
-  /** Declare varaibles */
-  int c;
+int main(void)
+{
+	/** Declare varaibles */
+	int c;
 
-  /** Set variables */
-  c = getchar();
+	/** Set variables */
+	c = getchar();
 
-  while (c != EOF){
-    putchar(c);
-    c = getchar();
-  }
-
-  return (0);
+	while (c != EOF)
+	{
+		putchar(c);
+		c = getchar();
+	}
+	return (0);
 }
