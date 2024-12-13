a9417741760189807a0842caf04efaf637c4dce3
BillClintonAglomasa <aglomasa@yahoo.com>
2024-12-13 16:27:34 +0000

    Rewrote the program to conform with betty standards

--- SE/C_programming/Chapter_1/ex3.3_print_value_EOF.c
+++ SE/C_programming/Chapter_1/ex3.3_print_value_EOF.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- * Verify the expression
- * getchar() != EOF is 0 or 1
+ * main - Entry point to program
+ *
+ * Return: Always return (0)
+ *
+ * Defintion: To Verify the expression, getchar() != EOF is 0 or 1
  */
 
 int main(void)
 {
-  /**Declaring variables*/
-  long int c;
+	/**Declaring variables*/
+	long int c;
 
-  /**Initializing the variables*/
-  c = getchar() != EOF;
-  printf("%ld\n", c);
+	/**Initializing the variables*/
+	c = getchar() != EOF;
+	printf("%ld\n", c);
 
-  return (0);
+	return (0);
 }
