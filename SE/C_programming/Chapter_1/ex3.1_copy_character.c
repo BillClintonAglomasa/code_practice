af2ec9455fee970173b92c622ea10b6f8ca78de6
BillClintonAglomasa <aglomasa@yahoo.com>
2024-12-13 16:19:55 +0000

    Rewrote code to conform with betty standard

--- SE/C_programming/Chapter_1/ex3.1_copy_character.c
+++ SE/C_programming/Chapter_1/ex3.1_copy_character.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 /**
- *Copy character and print it to screen */
+ * main - Entry to program
+ *
+ * Return: Always return (0)
+ *
+ * Definiton: Proram to copy character and print it to screen
+ */
 
-int main(void){
+int main(void)
+{
+/*Declare variables*/
+	int c;
 
-  /**
-   *Declare variables
-   */
-  int c;
+	while ((c = getchar()) != EOF)
+		putchar(c);
 
-  while ((c = getchar()) != EOF)
-    putchar(c);
-
-  return (0);
+	return (0);
 }
