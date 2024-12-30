7aad135b0886ab29e47d22f9baa26066933e5505
BillClintonAglomasa <aglomasa@yahoo.com>
2024-12-30 23:28:14 +0000

    A program to read lines of input and print the longest line

---
+++ SE/C_programming/Chapter_1/ex9.0_reads_lines_print_longest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MAXLINE 1000 /** Maximum input line size */
+/**
+ * main - Entry to program
+ *
+ * Return: Always 0
+ *
+ * Defintion: A program that read lines and prints out the longest of them
+ */
+
+int main(void)
+{
+	/** Declaration of variables */
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+}
