93ecad83917ff429b67a152424385e12347f4f6b
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-06 22:41:28 +0000

    Updated ex9.0

--- SE/C_programming/Chapter_1/ex9.0_reads_lines_print_longest.c
+++ SE/C_programming/Chapter_1/ex9.0_reads_lines_print_longest.c
@@ -15,4 +15,15 @@ int main(void)
 	int max;
 	char line[MAXLINE];
 	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	if (max > 0)
+		printf("%s", longest);
+	return 0;
 }
