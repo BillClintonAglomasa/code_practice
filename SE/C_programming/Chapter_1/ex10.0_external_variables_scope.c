f9210ade8d1f01de7d58ffd779ebec6dbcae4043
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-14 23:39:18 +0000

    Updated program with the main function

--- SE/C_programming/Chapter_1/ex10.0_external_variables_scope.c
+++ SE/C_programming/Chapter_1/ex10.0_external_variables_scope.c
@@ -6,5 +6,24 @@ int max;
 char line[MAXLINE];
 char longest[MAXLINE];
 
-int getline(void);
+int my_getline(void);
 void copy(void);
+
+int main(void)
+{
+	int len;
+	extern int max;
+	extern char longest[];
+
+	max = 0;
+	while ((len = my_getline()) > 0)
+		if (len > max)
+		{
+			max = len;
+			copy();
+		}
+
+	if (max > 0)
+		printf("%s", longest);
+	return 0;
+}
