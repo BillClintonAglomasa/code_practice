f2877297822522f27d9136195cbae7334d58a772
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-15 23:35:17 +0000

    Program to print the frequency of different character

---
+++ SE/C_programming/Chapter_1/ex7.2_print_frequency_chars.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/** Character length */
+#define CHAR_NUM 256
+
+/**
+ * main - Entry to program
+ *
+ * Return: Always 0
+ *
+ * Description: A program that prints the the frequency of a character
+ */
+
+int main(void)
+{
+	/** Declare variables */
+	int c;
+	char freq[CHAR_NUM];
+
+	/** Instantiate variables */
+	freq[CHAR_NUM] = {0};
+
+	while ((c = getchar() != EOF)
+		{
+			;
+		}
+	return (0);
+}
