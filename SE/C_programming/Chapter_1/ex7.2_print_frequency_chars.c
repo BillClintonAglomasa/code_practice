7b692a992f2aeb024e3dd40f5154a8e4fdc80ca8
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-16 00:10:37 +0000

    Corrected the error associated with array declaration and initialization

--- SE/C_programming/Chapter_1/ex7.2_print_frequency_chars.c
+++ SE/C_programming/Chapter_1/ex7.2_print_frequency_chars.c
@@ -15,27 +15,15 @@ int main(void)
 {
 	/** Declare variables */
 	int c;
-	char freq[CHAR_NUM];
 
 	/** Initialize variables */
-	freq[CHAR_NUM] = {0};
+	char freq[CHAR_NUM] = {0};
 
 	/** Check character and run if it is not EOF */
 	while ((c = getchar()) != EOF)
 		{
 			if (c >= 0 && c <= CHAR_NUM)
-			{
-				if (c >= 32 && c <= 128)
-				{
-					printf("%c: ", c);
-					++freq[c];
-				}
-				else
-				{
-					printf("0x%02X: ", c);
-					++freq[c];
-				}
-			}
+				++freq[c];
 		}
 
 	/** Printing frequency of unique characters */
