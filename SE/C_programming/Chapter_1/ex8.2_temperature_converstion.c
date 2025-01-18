62efa28c1b911dd647c629097425d509121ec13d
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-18 09:59:04 +0000

    Added function prototype of fahrtocel

--- SE/C_programming/Chapter_1/ex8.2_temperature_converstion.c
+++ SE/C_programming/Chapter_1/ex8.2_temperature_converstion.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#define CHAR_NUM 256
+
+/**
+ * fahrtocel - A function that converts temperature in fahrenheit to celsius
+ *
+ * @fahr: Temperature in fahrenheit
+ *
+ * Description: This function takes a float of the temperature in fahrenheit,
+ *              converts it to degree celsius and returns it as a float.
+ */
+
+float fahrtocel(float fahr);
+
 int main(void)
 {
-	int i;
-
 	return (0);
 }
