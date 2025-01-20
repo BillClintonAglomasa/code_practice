2bf6d1710c799689daa74dafe4839e45eb4a17af
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-20 23:26:58 +0000

    Added comments to improve clarity

--- SE/C_programming/Chapter_1/ex10.0_external_variables_scope.c
+++ SE/C_programming/Chapter_1/ex10.0_external_variables_scope.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+/** Maximum number of characters */
 #define MAXLINE 1000
 
+/** Declaration of external variables */
 int max;
 char line[MAXLINE];
 char longest[MAXLINE];
 
+/** Function prototype */
 int my_getline(void);
 void copy(void);
 
 int main(void)
 {
+	/** Declaration of variables */
 	int len;
 	extern int max;
 	extern char longest[];
 
+	/** Initialization */
 	max = 0;
+
 	while ((len = my_getline()) > 0)
 		if (len > max)
 		{
