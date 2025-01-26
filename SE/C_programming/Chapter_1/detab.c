0bc097323d55cf63163d8b15c223456510298ed8
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-26 22:03:38 +0000

    Renamed entab.c to detab.c

---
+++ SE/C_programming/Chapter_1/detab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0
+ *
+ * Description: This replaces tabs in the input with the proper number of blanks to sapce to the next tab stop.
+ */
+
+int main(void)
+{
+	return (0);
+}
