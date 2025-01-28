cff6cb9fb392d77a26204e233930151ae6715872
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-28 23:34:58 +0000

    Added an if clause to check for characters

--- SE/C_programming/Chapter_1/detab.c
+++ SE/C_programming/Chapter_1/detab.c
@@ -5,10 +5,22 @@
  *
  * Return: Always 0
  *
- * Description: This replaces tabs in the input with the proper number of blanks to sapce to the next tab stop.
+ * Description: This replaces tabs in the input with the proper number of blanks
+ *              to sapce to the next tab stop.
  */
 
 int main(void)
 {
+	/** Declaration of variables */
+	int c;
+
+	/** Initialization of variables */
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+			;
+		putchar(c);
+	}
 	return (0);
 }
