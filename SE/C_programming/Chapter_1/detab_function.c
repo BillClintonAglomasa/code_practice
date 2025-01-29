4fc1c6f08721b7681c66a45ccafb189ae575b062
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-29 15:47:25 +0000

    Wrote detab_function to replace tabs with spaces

---
+++ SE/C_programming/Chapter_1/detab_function.c
@@ -0,0 +1,41 @@
+/**
+ * detab_function - A function to replaces tabs with the right number of spaces
+ *
+ * Return: Nothing
+ *
+ * Description: This is a function that takes in an integer of the number of
+ *              spaces needed to replace a tab. It then moves the column to
+ *              the next stop by calculating the number from the previous tab
+ *              stop and then adds the required space to format the text.
+ */
+
+void detab_function(int TABSTOP)
+{
+	/** Declaration of variables */
+	int c, i, column_position, spaces;
+
+	/** Initialization of variables */
+	i = column_position = 0;
+
+	/** Check if character is not EOF */
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+		{
+			spaces = TABSTOP - (column_position % TABSTOP);
+			while (i < spaces)
+			{
+				putchar(' ');
+				++i;
+				++column_position;
+			}
+		}
+		else
+		  {
+			  putchar(c);
+			  ++column_position;
+			  if (c == '\n')
+				  column_position = 0;
+		  }
+	}
+}
