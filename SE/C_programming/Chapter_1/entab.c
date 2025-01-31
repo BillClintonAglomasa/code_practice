5fdf79661829a01f050dc0a7eb148d82ef9a3948
BillClintonAglomasa <aglomasa@yahoo.com>
2025-01-31 20:21:16 +0000

    Main program to run the entab_function

---
+++ SE/C_programming/Chapter_1/entab.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to program
+ *
+ * Return: Nothing
+ *
+ * Description: A program that uses entab_function to replace two or more blank
+ *              spaces with a tab.
+ */
+
+int main(void)
+{
+	return (0);
+}
