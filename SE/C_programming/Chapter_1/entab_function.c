c62841cfe5ca696560c18138201ec3fbf04a6555
BillClintonAglomasa <aglomasa@yahoo.com>
2025-02-03 22:33:36 +0000

    Added the function prototype of the entab_function

---
+++ SE/C_programming/Chapter_1/entab_function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * entab_function - A function that replaces blanks with tab
+ *
+ * @tabstop: An integer that represents the number of blanks that make a tabstop
+ *
+ * Return: Nothing
+ *
+ * Description: A function that takes an integer as a tabstop and uses that to
+ *              replace the number of blanks with a tab.
+ */
+
+void entab_funciton(int tabstop)
+{}
