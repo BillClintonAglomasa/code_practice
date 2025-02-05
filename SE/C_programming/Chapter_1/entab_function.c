df2aa208ff74c00f34487b9c8157f0c1e32a3ec8
BillClintonAglomasa <aglomasa@yahoo.com>
2025-02-05 14:40:57 +0000

    Updated the function to replace blanks with either tabs or spaces

--- SE/C_programming/Chapter_1/entab_function.c
+++ SE/C_programming/Chapter_1/entab_function.c
@@ -14,12 +14,13 @@
 void entab_funciton(int tabstop)
 {
 	/** Declaration of variables */
-	int c, column_position, blank_count;
+	int c, column_position, blank_count, spaces_to_tab;
 
 	/** Initialization of variables */
 	column_position = 0;
 	blank_count = 0;
 
+	/** If a character is not EOF */
 	while ((c = getchar()) != EOF)
 	{
 		if (c == ' ')
@@ -27,7 +28,55 @@ void entab_funciton(int tabstop)
 			blank_count++;
 		}
 
+		/** If character is not a blank */
 		else
-		{}
+		{
+			while (blank_count > 0)
+			{
+				/** Calculate the no of spaces to the next tab */
+				spaces_to_tab = tabstop - (column_position % tabstop);
+
+				if (blank_count >= spaces_to_tab)
+				{
+					putchar('\t');
+					column_position += spaces_to_tab;
+					blank_count -= spaces_to_tab;
+				}
+
+				else
+				{
+					putchar(' ');
+					column_position++;
+					blank_count--;
+				}
+			}
+
+			if (c == '\n')
+				column_position = 0;
+
+			else
+				column_position++;
+
+			putchar(c);
+		}
+	}
+
+	while (blank_count > 0)
+	{
+		spaces_to_tab = tabstop - (column_position % tabstop);
+
+		if (blank_count >= spaces_to_tab)
+		{
+			putchar('\t');
+			column_position += spaces_to_tab;
+			blank_count -= spaces_to_tab;
+		}
+
+		else
+		{
+			putchar(' ');
+			column_position++;
+			blank_count--;
+		}
 	}
 }
