a389a5f061401d2445a5e7c31f21f82af362828d
BillClintonAglomasa <aglomasa@yahoo.com>
2025-02-06 21:12:55 +0000

    A program to remove comments from C program

---
+++ SE/C_programming/Chapter_1/uncomment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to program
+ *
+ * Return: Nothing
+ *
+ * Description: A program that remove all comments from C program
+ */
+
+int main(void)
+{
+	return (0);
+}
