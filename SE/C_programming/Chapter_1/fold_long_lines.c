0dc8a35c47e3cd59e765a6a5a68345e4f5f55f7b
BillClintonAglomasa <aglomasa@yahoo.com>
2025-02-08 21:21:36 +0000

    Program to separate a line into two or more if a nth column is exceeded

---
+++ SE/C_programming/Chapter_1/fold_long_lines.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to program
+ *
+ * Return: Nothing
+ *
+ * Description: A program that breaks a an input line into two or more if an
+ *              nth column is exceeded.
+ */
+
+int main(void)
+{
+	return (0);
+}
