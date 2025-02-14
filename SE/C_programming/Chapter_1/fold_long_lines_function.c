0c37c4403af4665796bff370224b7672f14966dc
BillClintonAglomasa <aglomasa@yahoo.com>
2025-02-14 23:58:40 +0000

    Added an if clause to check for characters

--- SE/C_programming/Chapter_1/fold_long_lines_function.c
+++ SE/C_programming/Chapter_1/fold_long_lines_function.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define WORD_IN 1
+#define WORD_OUT 0
+
 /**
  * fold_long_lines - A function that fold long lines into two or more
  *
@@ -13,16 +16,30 @@
 void fold_long_lines(int m_count)
 {
 	/** Declaration of variables */
-	int c, column_count;
+	int c, column_count, state, word_count;
+	int current_line;
 
 	/** Initialization of variables */
 	column_count = 0;
+	word_count = 0;
 
 
 	/** Check if it is a line */
 	while ((c = getchar()) != EOF)
 		{
-			column_count++;
+			column_count++
+			if ((c >= 'a' || c <= 'z') && (c >= 'A' || c <= 'Z'))
+			{
+				state = WORD_IN;
+				word_count++;
+
+				/** Check if line exceeds 20 columns */
+				current_line = column_count + word_count;
+
+				if (current_line < m_count)
+					putchar(c);
+			}
+			
 			if ((c == '\n') && (column_count < m_count))
 			{
 				putchar('\n');
