eaa3b1a8b1cee1f62ffbe1ab719298ad836a3529
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-13 07:46:01 +0000

    Added a_srand function

--- SE/C_programming/Chapter_1/a_num_generator.c
+++ SE/C_programming/Chapter_1/a_num_generator.c
@@ -14,3 +14,16 @@ int a_rand(void)
 	next = next * 1103515245 + 12345;
 	return ((unsigned int) (next / 65536) % 32768);
 }
+
+/**
+ * a_srand - A function that provides a new value for next
+ *
+ * @seed: An integer that is passed to next
+ *
+ * Return: Nothing
+ */
+
+void a_srand(unsigned int seed)
+{
+	next = seed;
+}
