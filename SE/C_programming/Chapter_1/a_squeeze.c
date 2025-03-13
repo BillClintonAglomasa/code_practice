51542ecbd7ace27e8716d747877d35ab11ed007d
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-13 10:39:55 +0000

    A function that deletes a character from an array

---
+++ SE/C_programming/Chapter_1/a_squeeze.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * a_squeeze - A function that deletes a character from an array
+ *
+ * @c: A character that is to be deleted from the array
+ *
+ * Return: Nothing
+ */
+
+void a_squeeze(int c)
+{
+	/** Declaration of variables */
+	int i, j;
+
+	/** Initialization f variables */
+	i = j = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+			s[j++] = s[i];
+		i++;
+	}
+
+	s[j] = '\0';
+}
