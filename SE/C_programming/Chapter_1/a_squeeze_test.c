60fa3b047b2029d37c42c40d22e7f80bacc015ea
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-13 10:42:45 +0000

    A program to test the a_squeeze.c function

---
+++ SE/C_programming/Chapter_1/a_squeeze_test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/** Prototype declaration */
+void a_squeeze(char s[], int c);
+
+/**
+ * main - Entry to program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/** Declaration of variables */
+	int i, j, c;
+	char a[] = "clinton cocoyam";
+
+	/** Initialization of variables */
+	i = j = 0;
+	c = 'c';
+
+	/** Before modification of array */
+	putchar('\n');
+
+	while (a[i] != '\0')
+	{
+		printf("%c", a[i]);
+		i++;
+	}
+
+	/** Modification of array */
+	a_squeeze(a[], c);
+
+	/** After modification of array */
+	putchar('\n');
+	while (a[j] != '\0')
+	{
+		printf("%c", a[j]);
+		j++;
+	}
+
+	return (0);
+}
