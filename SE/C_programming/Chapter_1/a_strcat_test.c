252f305cc33faee6ec718a000695af4b2d67efcf
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-14 23:32:35 +0000

    A program to test the a_strcat function

---
+++ SE/C_programming/Chapter_1/a_strcat_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/** Function prototype */
+void a_strcat(char a[], char b[]);
+
+/**
+ * main - Entry of program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/** Declaration and initialization */
+	int i = 0;
+
+	char c[] = "Bill Clinton Aglomasa";
+	char d[] = "is my name.";
+
+	a_strcat(c, d);
+
+	putchar('\n');
+
+	while (c[i] != '\0')
+	{
+		printf("%c", c[i]);
+		i++;
+	}
+
+	return (0);
+}
