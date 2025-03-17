9269eaae2f9573a88afc06a14e84fe7f188dfafd
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-17 20:43:16 +0000

    A function to deletes a character in an array that matches a character in another array

---
+++ SE/C_programming/Chapter_1/a_squeeze_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * a_squeeze_1 - A function that deletes each character in an array that matches
+                 any character in another array.
+ *
+ * @s1: A string array
+ * @s2: A string array
+ *
+ * Return: Nothing
+ */
+
+void a_squeeze_1(char s1[], char s2[])
+{
+	
+}
