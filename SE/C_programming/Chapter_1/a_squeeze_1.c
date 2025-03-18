421bac5bd5369bea0c63513a3e21adc28f5f5691
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-18 16:16:44 +0000

    Updated function to delete characters from a string

--- SE/C_programming/Chapter_1/a_squeeze_1.c
+++ SE/C_programming/Chapter_1/a_squeeze_1.c
@@ -2,7 +2,7 @@
 
 /**
  * a_squeeze_1 - A function that deletes each character in an array that matches
-                 any character in another array.
+ *               any character in another array.
  *
  * @s1: A string array
  * @s2: A string array
@@ -12,5 +12,19 @@
 
 void a_squeeze_1(char s1[], char s2[])
 {
-	
+	/** Declaration of variables */
+	int i, j;
+
+	/** Initialization of variables */
+	i = j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+
+		if (s1[i] != s2[j])
+			j++;
+
+		s1[i] = '\0';
+	}
 }
