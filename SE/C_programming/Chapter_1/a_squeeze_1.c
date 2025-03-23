4a23bc91d42d498fed1cfca42ac388cccd550ffe
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-23 01:28:56 +0000

    Updated a_squeeze_1.c function

--- SE/C_programming/Chapter_1/a_squeeze_1.c
+++ SE/C_programming/Chapter_1/a_squeeze_1.c
@@ -13,28 +13,32 @@
 void a_squeeze_1(char s1[], char s2[])
 {
 	/** Declaration of variables */
-	int i, j, k;
+	int i, j, k, found;
 
 	/** Initialization of variables */
-	i = 0;
-	k = 0;
+	i = j = k = found = 0;
 
 	while (s1[i] != '\0')
 	{
 		j = 0;
+
 		while (s2[j] != '\0')
 		{
-			if (s1[i] != s2[j])
+			if (s1[i] == s2[j])
 			{
-				s1[k] = s1[i];
-				k++;
+				found = 1;
+				break;
 			}
-
 			j++;
+		}
 
+		if (!found)
+		{
+			s1[k++] = s1[i];
 		}
 
 		i++;
+
 	}
 
 	s1[k] = '\0';
