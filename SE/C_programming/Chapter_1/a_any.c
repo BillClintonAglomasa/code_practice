fd53839d2f41fa4b534a0f60895b4353f9d0c67d
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-24 22:55:39 +0000

    A function that returns an integer if there is a match or -1 if there is no match

---
+++ SE/C_programming/Chapter_1/a_any.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * a_any - A function which returns the first location in a string when it
+ *         matches another character or -1 if no characters match in another
+ *         string.
+ *
+ * @s1: An array of characters
+ * @s2: An array of characters
+ *
+ * Return: The location of a string in s1 if it matches a character in s2 or
+ *         -1 if there are no matches.
+ */
+
+int a_any(s1[], s2[])
+{
+	/** Declaration of variables */
+	int i, j, found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			found = 0;
+
+			if (s[i] == s[j])
+			{
+				found = 1;
+				return (i);
+			}
+		}
+
+		if (!found)
+			return (-1);
+	}
+}
