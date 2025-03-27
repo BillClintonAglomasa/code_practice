4d792aff46b66a73a9ae5cb53b68472105405d1c
BillClintonAglomasa <aglomasa@yahoo.com>
2025-03-27 19:23:09 +0000

    Deleted all intermediate files

--- SE/C_programming/Chapter_1/a_any_test.c
+++ SE/C_programming/Chapter_1/a_any_test.c
@@ -15,14 +15,14 @@ int main(void)
 	int i, j;
 
 	/** Initialization of arrays */
-	q1[] = "This is Bill Clinton";
-	q2[] = "Is he really Bill Clinton?";
-	q3[] = "Mmmm";
+	char q1[] = "This is Bill Clinton";
+	char q2[] = "Is he really Bill Clinton?";
+	char q3[] = "Mmmm";
 
 	/** Printing strings */
-	printf("%s", q1);
-	printf("%s", q2);
-	printf("%s", q3);
+	printf("%s\n", q1);
+	printf("%s\n", q2);
+	printf("%s\n", q3);
 
 	i = a_any(q1, q2);
 	j = a_any(q2, q3);
@@ -30,8 +30,8 @@ int main(void)
 	/** Print values after function call */
 	putchar('\n');
 
-	printf("%d", i);
-	printf("%d", j);
+	printf("%d\n", i);
+	printf("%d\n", j);
 
 	return (0);
 }
