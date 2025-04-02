215eb6de31475fc4630e19e709af2f7b6e13cd13
BillClintonAglomasa <aglomasa@yahoo.com>
2025-04-02 19:02:13 +0000

    A program to test a_setbit function.

---
+++ SE/C_programming/Chapter_1/a_setbit_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/** Prototype declaration */
+int a_setbit(int a);
+
+/**
+ * main - Entry to the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/** Declaration of variables */
+	int a, b;
+
+	/** Initialization of variables */
+	a = 10;
+
+	/** Calling function */
+	b = a_setbit(a);
+
+	/** Printing value of b */
+	putchar('\n');
+	putchar(b);
+
+	return (0);
+}
