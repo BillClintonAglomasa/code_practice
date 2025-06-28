0d308a4f4f5ce97e9ad87ef0f62392eb5013ab5a
BillClintonAglomasa <aglomasa@yahoo.com>
2025-06-28 23:46:19 +0000

    Printing out variable values

---
+++ SE/Kochan/Chapter_4/prog_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to program
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int integerVar = 100;
+	float floatingVar = 331.79;
+	double doubleVar = 8.44e+11;
+	char charVar = "W";
+
+	_Bool boolvar = 0;
+
+
+	printf("integerVar = %i\n", integerVar);
+
+
+	return (0);
+}
