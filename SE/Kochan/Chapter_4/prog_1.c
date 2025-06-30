e779cb6f0d60ceed6510c2704f142cc51947e324
BillClintonAglomasa <aglomasa@yahoo.com>
2025-06-30 01:54:36 +0000

    Updated prog_1.c to print other datatypes.

--- SE/Kochan/Chapter_4/prog_1.c
+++ SE/Kochan/Chapter_4/prog_1.c
@@ -10,12 +10,16 @@ int main(void)
 	int integerVar = 100;
 	float floatingVar = 331.79;
 	double doubleVar = 8.44e+11;
-	char charVar = "W";
+	char charVar = 'W';
 
 	_Bool boolvar = 0;
 
 
 	printf("integerVar = %i\n", integerVar);
+	printf("floatingVar = %f\n", floatingVar);
+	printf("doubleVar = %e\n", doubleVar);
+	printf("doubleVar = %g\n", doubleVar);
+	printf("integerVar = %c\n", charVar);
 
 
 	return (0);
