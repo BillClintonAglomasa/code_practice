e29ea334f99a29737b1137fd1ed24d05aed35e87
BillClintonAglomasa <aglomasa@yahoo.com>
2025-07-03 09:02:37 +0000

    Program that implements the usage of the modulus operator.

---
+++ SE/Kochan/Chapter_4/prog_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry to program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 25, b = 5, c = 10, d = 7;
+
+	printf("a %% b = %i\n", a % b);
+
+	return (0);
+}
